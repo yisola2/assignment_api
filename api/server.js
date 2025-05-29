ae71a7692e35a0d1e95b980f2ac2f665f629245f
Yassin <yassyisola@gmail.com>
2025-05-29 19:31:44 +0200

    fixes for token

--- api/server.js
+++ api/server.js
@@ -29,7 +29,7 @@ mongoose.connect(uri, options)
 // CORS middleware
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, X-API-Token");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
 });
